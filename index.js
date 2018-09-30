79d38d9bddc7e6435ebcce44effaf86937df04d0
Christopher Brown <io@henrian.com>
2018-09-30 11:30:04 -0500

    Remove semicolons

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-var http = require('http');
-var querystring = require('querystring');
-var url = require('url');
+var http = require('http')
+var querystring = require('querystring')
+var url = require('url')
 
 function _serialize(object) {
   try {
-    return JSON.stringify(object);
+    return JSON.stringify(object)
   }
   catch (error) {
-    return JSON.stringify(ErrorResult.fromError(error));
+    return JSON.stringify(ErrorResult.fromError(error))
   }
 }
 
@@ -17,174 +17,174 @@ var ErrorResult = function(message, name) {
 
   Every Error has a .message. Most Errors have a .name. Anything else is optional.
   */
-  this.message = message;
-  this.name = name || 'Error';
-};
+  this.message = message
+  this.name = name || 'Error'
+}
 ErrorResult.fromError = function(error) {
-  var error_result = new ErrorResult(error.message, error.name);
+  var error_result = new ErrorResult(error.message, error.name)
   for (var key in error) {
     if (error.hasOwnProperty(key)) {
-      error_result[key] = error[key];
+      error_result[key] = error[key]
     }
   }
-  return error_result;
-};
+  return error_result
+}
 ErrorResult.prototype.toString = function() {
-  return this.name + ': ' + this.message;
-};
+  return this.name + ': ' + this.message
+}
 
 // Request
 http.IncomingMessage.prototype.readToEnd = function(encoding, callback) {
-  // req.readToEnd([encoding], callback);
+  // req.readToEnd([encoding], callback)
   // callback signature: function(err, Buffer | null | String)
   // this basically has {decodeStrings: true}, even if we send it an encoding
 
   // encoding is optional, adjust if we only got less than two arguments
   if (callback === undefined) {
-    callback = encoding;
-    encoding = undefined;
+    callback = encoding
+    encoding = undefined
   }
   // encoding may still be undefined, in which case
   // we will just return a buffer, if callback is specifed
 
-  var self = this;
+  var self = this
 
   // is this closure unnecessarily messy?
   var success = function() {
-    var buffer = self._readToEnd_buffer;
-    callback(null, encoding ? buffer.toString(encoding) : buffer);
-  };
+    var buffer = self._readToEnd_buffer
+    callback(null, encoding ? buffer.toString(encoding) : buffer)
+  }
 
   // _readToEnd_state: undefined -> 'reading' -> 'ended'
   if (this._readToEnd_state === 'ended') {
-    if (callback) setImmediate(success);
+    if (callback) setImmediate(success)
   }
   else if (this._readToEnd_state === 'reading') {
-    if (callback) this.on('end', success);
+    if (callback) this.on('end', success)
   }
   else {
     // initialize
-    this._readToEnd_state = 'reading';
-    this._readToEnd_buffer = new Buffer(0);
+    this._readToEnd_state = 'reading'
+    this._readToEnd_buffer = new Buffer(0)
 
     // start listening for 'readable' events.
     this.on('readable', function() {
-      var chunk = self.read();
+      var chunk = self.read()
       if (chunk !== null) {
         if (!Buffer.isBuffer(chunk)) {
           // the user should really not be using req.setEncoding('whatever'),
           //   but we don't want to crash if they do
-          chunk = new Buffer(chunk);
+          chunk = new Buffer(chunk)
         }
-        self._readToEnd_buffer = Buffer.concat([self._readToEnd_buffer, chunk]);
+        self._readToEnd_buffer = Buffer.concat([self._readToEnd_buffer, chunk])
       }
-    });
+    })
 
     if (callback) {
-      this.on('error', callback);
+      this.on('error', callback)
     }
 
     // can 'error' and 'end' both be called? that might be bad.
     this.on('end', function() {
-      self._readToEnd_state = 'ended';
-      if (callback) success();
-    });
+      self._readToEnd_state = 'ended'
+      if (callback) success()
+    })
   }
-};
+}
 http.IncomingMessage.prototype.readData = function(callback) {
   /** Opinionated input/form reader
 
   callback signature: function(err, Object)
   */
   if (this.method == 'GET') {
-    var data = url.parse(this.url, true).query;
+    var data = url.parse(this.url, true).query
     setImmediate(function() {
-      callback(null, data);
-    });
+      callback(null, data)
+    })
   }
   else {
-    var content_type = this.headers['content-type'] || '';
+    var content_type = this.headers['content-type'] || ''
     this.readToEnd(function(err, body) {
-      if (err) return callback(err);
+      if (err) return callback(err)
 
       if (content_type.match(/application\/json/i)) {
         // empty body translates to null
         if (body.length === 0) {
-          callback(null, null);
+          callback(null, null)
         }
         else {
           try {
-            callback(null, JSON.parse(body));
+            callback(null, JSON.parse(body))
           }
           catch (exc) {
-            callback(exc);
+            callback(exc)
           }
         }
       }
       else if (content_type.match(/application\/x-www-form-urlencoded/i)) {
         // will querystring.parse ever throw?
-        var body_string = body.toString(); // assumes utf-8
-        callback(null, querystring.parse(body_string));
+        var body_string = body.toString() // assumes utf-8
+        callback(null, querystring.parse(body_string))
       }
       else {
-        callback(null, body);
+        callback(null, body)
       }
-    });
+    })
   }
-};
+}
 
 // Response
 http.ServerResponse.prototype.status = function(http_status_code) {
-  this.statusCode = http_status_code;
-  return this;
-};
+  this.statusCode = http_status_code
+  return this
+}
 http.ServerResponse.prototype.ngjson = function(object) {
   // angular.js style protection
-  this.setHeader('Content-Type', 'application/json');
+  this.setHeader('Content-Type', 'application/json')
   // angular.js will trim it either with or without the comma.
-  this.end(")]}',\n" + _serialize(object));
-  return this;
-};
+  this.end(")]}',\n" + _serialize(object))
+  return this
+}
 http.ServerResponse.prototype.xjson = function(object) {
   // this is how facebook does it, which is similar to how google does it (with 'while(1);')
   // I've also seen 'throw 1;' and '%%%BREAK%%%'
-  this.setHeader('Content-Type', 'application/x-javascript');
-  this.end('for (;;);' + _serialize(object));
-  return this;
-};
+  this.setHeader('Content-Type', 'application/x-javascript')
+  this.end('for (;;);' + _serialize(object))
+  return this
+}
 http.ServerResponse.prototype.json = function(object) {
-  this.setHeader('Content-Type', 'application/json');
-  this.end(_serialize(object));
-  return this;
-};
+  this.setHeader('Content-Type', 'application/json')
+  this.end(_serialize(object))
+  return this
+}
 http.ServerResponse.prototype.html = function(body) {
-  this.setHeader('Content-Type', 'text/html');
-  this.end(body);
-  return this;
-};
+  this.setHeader('Content-Type', 'text/html')
+  this.end(body)
+  return this
+}
 http.ServerResponse.prototype.text = function(body) {
-  this.setHeader('Content-Type', 'text/plain');
-  this.end(body);
-  return this;
-};
+  this.setHeader('Content-Type', 'text/plain')
+  this.end(body)
+  return this
+}
 http.ServerResponse.prototype.redirect = function(location) {
   if (this.statusCode == 200) {
     // only set the statusCode if it's the default
     // so, you would write res.status(307).redirect(url) if you want something besides 302
-    this.statusCode = 302;
+    this.statusCode = 302
   }
-  this.setHeader('Location', location);
-  this.end('Redirecting to: ' + location);
-  return this;
-};
+  this.setHeader('Location', location)
+  this.end('Redirecting to: ' + location)
+  return this
+}
 http.ServerResponse.prototype.die = function(error) {
   if (this.statusCode == 200) {
     // only reset an OK
-    this.statusCode = 500;
+    this.statusCode = 500
   }
-  var message = error ? 'Failure: ' + error.toString() : 'Failure';
-  return this.text(message);
-};
+  var message = error ? 'Failure: ' + error.toString() : 'Failure'
+  return this.text(message)
+}
 http.ServerResponse.prototype.error = function(error, request_headers) {
   /** Respond to the client with an error.
 
@@ -195,15 +195,15 @@ http.ServerResponse.prototype.error = function(error, request_headers) {
   */
   if (error.statusCode) {
     // if the error specifies the desired HTTP status code, use that.
-    this.statusCode = error.statusCode;
+    this.statusCode = error.statusCode
   }
   else if (this.statusCode == 200) {
     // if the error is not HTTP-aware, and the statusCode hasn't already been
     // set to something besides 200 OK, set it to the default error status of 400.
-    this.statusCode = 400;
+    this.statusCode = 400
   }
-  return this.adapt(ErrorResult.fromError(error), request_headers);
-};
+  return this.adapt(ErrorResult.fromError(error), request_headers)
+}
 http.ServerResponse.prototype.adapt = function(result, request_headers) {
   /** Using the Accept header from the accompanying request, serve an
   appropriate response to the client.
@@ -220,16 +220,16 @@ http.ServerResponse.prototype.adapt = function(result, request_headers) {
   TODO: support other mime types
   */
   // accept is (should be) a comma-separated list of mime types
-  var accept = 'text/plain';
+  var accept = 'text/plain'
   if (request_headers && request_headers.accept !== undefined) {
-    accept = request_headers.accept;
+    accept = request_headers.accept
   }
 
   // prefer JSON, if the client accepts it (see TODO in docstring)
   if (accept.indexOf('application/json') > -1) {
-    return this.json(result);
+    return this.json(result)
   }
-  return this.text(result.toString());
-};
+  return this.text(result.toString())
+}
 
-module.exports = http;
+module.exports = http
--- test/server.js
+++ test/server.js
@@ -1,115 +1,115 @@
-var tap = require('tap');
-var request = require('request');
+var tap = require('tap')
+var request = require('request')
 
-var http = require('..');
+var http = require('..')
 
 var handler = function(req, res) {
   if (req.url == '/empty') {
-    res.status(204).end();
+    res.status(204).end()
   }
   else if (req.url == '/sample.ngjson') {
-    res.ngjson({name: 'Chris'});
+    res.ngjson({name: 'Chris'})
   }
   else if (req.url == '/sample.xjson') {
-    res.xjson({name: 'Chris'});
+    res.xjson({name: 'Chris'})
   }
   else if (req.url == '/sample.json') {
-    res.json({name: 'Chris'});
+    res.json({name: 'Chris'})
   }
   else if (req.url == '/body.html') {
-    res.html('<h1>Hello tests!</h1>');
+    res.html('<h1>Hello tests!</h1>')
   }
   else if (req.url == '/plain.txt') {
-    res.text('Hello test suite.');
+    res.text('Hello test suite.')
   }
   else if (req.url == '/relocator') {
-    res.redirect('/elsewhere');
+    res.redirect('/elsewhere')
   }
   else if (req.url == '/input') {
     req.readToEnd('utf8', function(err, body) {
-      res.text(body.length.toString());
-    });
+      res.text(body.length.toString())
+    })
   }
   else if (req.url == '/extract-id') {
     req.readData(function(err, data) {
-      res.text(data.id.toString());
-    });
+      res.text(data.id.toString())
+    })
   }
   else if (req.url == '/extract-title') {
     req.readData(function(err, data) {
-      res.text(data.title);
-    });
+      res.text(data.title)
+    })
   }
   else {
-    res.die('Not found, dammit!');
+    res.die('Not found, dammit!')
   }
-};
+}
 
 tap.test('basic server', function(t) {
-  t.plan(21);
+  t.plan(21)
 
-  var hostname = '127.0.0.1';
+  var hostname = '127.0.0.1'
   // port = 0 finds random free port
   var server = http.createServer(handler).listen(0, hostname, function() {
     // server running, ready to run tests
-    var port = server.address().port;
-    var addrport = 'http://' + hostname + ':' + port;
+    var port = server.address().port
+    var addrport = 'http://' + hostname + ':' + port
 
     request.get(addrport + '/empty', function(err, res, body) {
-      t.equal(res.statusCode, 204);
-      t.equal(body, '');
-    });
+      t.equal(res.statusCode, 204)
+      t.equal(body, '')
+    })
 
     request.get(addrport + '/sample.ngjson', function(err, res, body) {
-      t.equal(res.headers['content-type'], 'application/json');
-      t.equal(res.statusCode, 200);
-      t.equal(body, ')]}\',\n{"name":"Chris"}');
-    });
+      t.equal(res.headers['content-type'], 'application/json')
+      t.equal(res.statusCode, 200)
+      t.equal(body, ')]}\',\n{"name":"Chris"}')
+    })
 
     request.get(addrport + '/sample.xjson', function(err, res, body) {
-      t.equal(res.statusCode, 200);
-      t.equal(body, 'for (;;);{"name":"Chris"}');
-    });
+      t.equal(res.statusCode, 200)
+      t.equal(body, 'for (;;);{"name":"Chris"}')
+    })
 
     request.get(addrport + '/body.html', function(err, res, body) {
-      t.equal(res.headers['content-type'], 'text/html');
-      t.equal(body, '<h1>Hello tests!</h1>');
-    });
+      t.equal(res.headers['content-type'], 'text/html')
+      t.equal(body, '<h1>Hello tests!</h1>')
+    })
 
     request.get(addrport + '/plain.txt', function(err, res, body) {
-      t.equal(res.headers['content-type'], 'text/plain');
-      t.equal(body, 'Hello test suite.');
-    });
+      t.equal(res.headers['content-type'], 'text/plain')
+      t.equal(body, 'Hello test suite.')
+    })
 
     request.get(addrport + '/null-and-void', function(err, res, body) {
-      t.equal(res.statusCode, 500);
-      t.equal(body, 'Failure: Not found, dammit!');
-    });
+      t.equal(res.statusCode, 500)
+      t.equal(body, 'Failure: Not found, dammit!')
+    })
 
     request.get(addrport + '/relocator', {followRedirect: false}, function(err, res, body) {
-      t.equal(res.statusCode, 302);
-      t.equal(body, 'Redirecting to: /elsewhere');
-    });
+      t.equal(res.statusCode, 302)
+      t.equal(body, 'Redirecting to: /elsewhere')
+    })
 
-    var input_body = 'This is only a test, so chill.';
+    var input_body = 'This is only a test, so chill.'
     request.post(addrport + '/input', {body: input_body}, function(err, res, body) {
-      t.equal(res.statusCode, 200);
-      t.equal(body, input_body.length.toString());
-    });
+      t.equal(res.statusCode, 200)
+      t.equal(body, input_body.length.toString())
+    })
 
-    var extract = {id: 'king', title: 'tortuga'};
+    var extract = {id: 'king', title: 'tortuga'}
     request.post(addrport + '/extract-id', {form: extract}, function(err, res, body) {
-      t.equal(res.statusCode, 200);
-      t.equal(body, 'king');
-    });
+      t.equal(res.statusCode, 200)
+      t.equal(body, 'king')
+    })
 
     request.post(addrport + '/extract-title', {json: extract}, function(err, res, body) {
-      t.equal(res.statusCode, 200);
-      t.equal(body, 'tortuga');
-    });
-  });
+      t.equal(res.statusCode, 200)
+      t.equal(body, 'tortuga')
+    })
+  })
 
   t.tearDown(function() {
-    server.close();
-  });
-});
+    server.close()
+  })
+})
