daa6be5b211023f075936092a3fc45659c35b754
Christopher Brown <io@henrian.com>
2018-09-30 11:16:32 -0500

    Delete import tests

--- test/import.js
+++
@@ -1,8 +0,0 @@
-/*jslint node: true */
-var tap = require('tap');
-
-tap.test('require should substantiate', function(t) {
-  var http = require('../');
-  t.ok(http, 'http should load from index.js in parent directory');
-  t.end();
-});
